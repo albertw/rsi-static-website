9a8c46424a96d557146990afbf2a1d9475689ded
albertw <albertw@users.noreply.github.com>
2026-05-19 07:20:51 +0000

    deploy: 3b9260ad467bb7db6a1a51e080eddc656ea785f5

--- game-config.js
+++ game-config.js
@@ -4,6 +4,6 @@ var gameConfig = window.gameConfig || {
   storage_mode: "auto",
   event_id: "global",
   event_name: "Global Leaderboard",
-  supabase_url: "",
-  supabase_key: ""
+  supabase_url: "https://ekocknufnhamymnxthjs.supabase.co",
+  supabase_key: "sb_publishable_epqI6n5MJDpPPMNt1al1aw_IreQYWdA"
 };
